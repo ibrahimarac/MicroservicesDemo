fa91dbbac648603866fddba6e70d72fc0910d315
Halil Ibrahim ARAC <hiarac@karatekin.edu.tr>
2022-01-05 11:42:12 +0300

    Rapor, Rapor Durum, Kisi ve Iletisim entity'ler tamamlandı.

--- src/Core/ContactReport.Domain/Entities/Contacts/IletisimBilgisi.cs
+++ src/Core/ContactReport.Domain/Entities/Contacts/IletisimBilgisi.cs
@@ -1,14 +1,15 @@
 ﻿
 using ContactReport.Domain.Common;
+using System;
 
-namespace ContactReport.Domain.Entities
+namespace ContactReport.Domain.Entities.Contacts
 {
-    public class IletisimBilgisi:BaseEntity<string>
+    public class IletisimBilgisi:BaseEntity<Guid>
     {
-        public string KisiId { get; set; }
+        public Guid KisiId { get; set; }
         public Kisi Kisi { get; set; }
 
-        public string BilgiTurId { get; set; }
+        public Guid BilgiTurId { get; set; }
         public IletisimBilgiTuru IletisimBilgiTuru { get; set; }
 
         public string BilgiIcerigi { get; set; }
---
+++ src/Core/ContactReport.Domain/Entities/Reports/RaporDurum.cs
@@ -0,0 +1,13 @@
+﻿using ContactReport.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace ContactReport.Domain.Entities.Reports
+{
+    public class RaporDurum:BaseEntity<Guid>
+    {
+        public string Durum { get; set; }
+
+        public ICollection<Rapor> Raporlar { get; set; }
+    }
+}
