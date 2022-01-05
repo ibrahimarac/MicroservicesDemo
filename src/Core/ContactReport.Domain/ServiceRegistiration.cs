efc16f3d6aa0cce874588d399c5f8102ac9cbc2b
Halil Ibrahim ARAC <hiarac@karatekin.edu.tr>
2022-01-06 01:03:32 +0300

    Dependency injection servisleri DI container'a aktaracak extension metodlar yazıldı.

---
+++ src/Core/ContactReport.Domain/ServiceRegistiration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactReport.Domain
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddDomainServices(this IServiceCollection services)
+        {
+            services.Ad
+        }
+    }
+}
---
+++ src/Infrastructure/ContactReport.Persistence/ServiceRegistiration.cs
@@ -0,0 +1,23 @@
+﻿
+using ContactReport.Application.Common.Interfaces;
+using ContactReport.Infrastructure.Persistence;
+using ContactReport.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactReport.Infrastructure
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ContactDbContext>(
+                    opt => opt.UseNpgsql("User ID=posgres;Password=password;Server=localhost;Port=5432;Database=ContactDB;Integrated Security=true;Pooling=true")
+                );
+
+            services.AddTransient<IExcelFileBuilder, ExcelFileBuilder>();
+        }
+
+    }
+}
