2e4e07992bec5bf7f2c7d13b578173467d666390
Halil Ibrahim ARAC <hiarac@karatekin.edu.tr>
2022-01-06 17:04:18 +0300

    Rapor talebi için gereken Query yazıldı

---
+++ src/Services/ReportAPI/Report.Application/CommandQueries/RaporIslemleri/Queries/RaporTalep/RaporTalepQuery.cs
@@ -0,0 +1,37 @@
+﻿using Karatekin.Web.Api.Core.Utilities.Result;
+using MediatR;
+using Report.Application.Interfaces.Repositories;
+using Report.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Report.Application.CommandQueries.RaporIslemleri.Queries.RaporTalep
+{
+    public class RaporTalepQuery:IRequest<Response>
+    {
+        public string Konum { get; set; }
+    }
+
+    public class RaporTalepQueryHandler : IRequestHandler<RaporTalepQuery, Response>
+    {
+        private readonly IRaporRepository _raporRepository;
+
+        public RaporTalepQueryHandler(IRaporRepository raporRepository)
+        {
+            _raporRepository = raporRepository;
+        }
+
+        public async Task<Response> Handle(RaporTalepQuery request, CancellationToken cancellationToken)
+        {
+            //rapor talebi geldiğinden hemen yeni bir kayıt açarak durumunu hazırlanıyor olarak ayarlayalım.
+            var rapor = new Rapor
+            {
+                Id=Guid.NewGuid(),
+                DurumId= new Guid("54a0bfc0-d88d-4e75-a067-9547e977c926")
+            };
+            await _raporRepository.Add(rapor);
+            return new SuccessDataResponse<Guid>(rapor.Id);
+        }
+    }
+}
---
+++ src/Services/ReportAPI/Report.Application/CommandQueries/RaporIslemleri/Queries/RaporTalep/RaporTalepQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Report.Application.CommandQueries.RaporIslemleri.Queries.RaporTalep
+{
+    public class RaporTalepQueryValidator:AbstractValidator<RaporTalepQuery>
+    {
+        public RaporTalepQueryValidator()
+        {
+            RuleFor(v => v.Konum)
+                .NotEmpty().WithMessage("Konum bilgisi zorunlu bir alandır.");
+        }
+    }
+}
